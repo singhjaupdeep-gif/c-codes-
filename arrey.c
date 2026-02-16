757a7e3f7536319754c562a2588c26b9cad1fe52
singhjaupdeep-git <singhjaupdeep@gmail.com>
2026-02-16 12:02:02 +0530

    array started

--- arrey.c
+++ arrey.c
@@ -4,26 +4,21 @@ int main(){
     do{
         printf("\n1.Create data");
         printf("\n2.Display data");
-        printf("\n3.Switch data");
-        printf("\n4.create data");
-    } while ();
-    
-
-
-
-
-
-
-
-    printf("\nenter no of elements :");
-    scanf("%d",&n);
-    for (int i=0; i<n ; i++){
-        printf("\nEnter no of elements:",i+1);
-        scanf("%d",&arr[i]);
-    }
-    printf("\nArrey elements are:");
-    for(int i=0; i<n; i++){
-        printf("%d ",arr[i]);
-    }
+        printf("\n3.Enter your choice");
+        scanf("%d",&choice);
+        switch (choice){
+            case 0:
+            break;
+            case 1:
+            printf("\nenter no of elements :");
+            scanf("%d",&n);
+            for (int i=0; i<n ; i++){
+                printf("\nEnter no of elements:",i+1);
+                scanf("%d",&arr[i]);
+            }
+            printf("\nArrey elements are:");
+            for(int i=0; i<n; i++){
+                printf("%d ",arr[i]);
+            }
     return 0;
 }
