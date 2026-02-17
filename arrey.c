779bd832f7b09300e0f6650da0e7dd127c0d18fa
singhjaupdeep-git <singhjaupdeep@gmail.com>
2026-02-17 11:27:11 +0530

    arrey printing

--- arrey.c
+++ arrey.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-    int arr[20], n, choice;
+    int arr[20], n, choice ,m;
     do{
         printf("\n1.Create data");
         printf("\n2.Display data");
@@ -8,9 +8,9 @@ int main(){
         scanf("%d", &choice);
         switch (choice)
         {
-        case 0:
+            case 0:
             break;
-        case 1:
+            case 1:
             printf("\nenter no of elements :");
             scanf("%d", &n);
             for (int i = 0; i < n; i++)
@@ -18,12 +18,28 @@ int main(){
                 printf("\nEnter no of elements:", i + 1);
                 scanf("%d", &arr[i]);
             }
-        case 2:
+            case 2:
             printf("\nArrey elements are:");
             for (int i = 0; i < n; i++)
             {
                 printf("%d ", arr[i]);
             }
+            case 3:
+            printf("\n count: %d",n);
+            case 4:
+            printf("\nEnter no of elements that you want to append:");
+            scanf("%d",&m);
+            for(int i=0; i<m+n; i++){
+                printf("\nEnter elements no. %d:",i+1);
+                scanf("%d",&arr[i]);
+            }
+            n=n+m;
+            break;
+            case 5:
+            printf("\nEnter the position");
+            scanf("%d",&pos);
+            
+
         }}
         while (choice != 0)
             ;
